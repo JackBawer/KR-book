bc7666af9a3921fb67c77691bed18b918f0f21c2
Jack Bawer <jackb2507@gmail.com>
2026-02-08 21:48:42 +0100

    .gitignore should know work

--- ch1/test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h> 
-
-int main(void) 
-{ 
-    printf("%zu\n", sizeof(long double));
-} 
--- ch5/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    int x = 42; 
-    int *ip;
-    ip = &x;
-    printf("The address of x is: %p\n", (void*) ip);
-    printf("The value of x is: %d\n", *ip);
-}
